9adffd792a7a1a8e733436eb1752a1d008d3c2fa
Idriss Khaled <79480567+idrisskhaled@users.noreply.github.com>
2023-12-01 00:56:08 +0100

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -22,4 +22,5 @@ provider "azurerm" {
   client_id = "d196182c-349c-4155-9e9b-8cf25a9408c1"
   client_secret = "5aQ8Q~vARR18Rqyd6MtQCF~wX3yAbQMbvfqP1bSz"
   tenant_id = "dbd6664d-4eb9-46eb-99d8-5c43ba153c61"
+  subscription_id = "e17aee72-105c-4866-a453-005f07809bd2"
 }
