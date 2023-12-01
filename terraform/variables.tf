8b4535b0a8e5462d2e62cce7a210c7dccb1ee0ba
Idriss Khaled <79480567+idrisskhaled@users.noreply.github.com>
2023-12-01 13:26:44 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,11 @@
-variable "resource_group_location" {
+variable "location" {
+  description = "The location/region in which to create the resources."
   type        = string
-  default     = "eastus"
-  description = "Location of the resource group."
+  default     = "West Europe"
 }
 
-variable "resource_group_name_prefix" {
+variable "resource_group_name" {
+  description = "The name of the resource group."
   type        = string
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
-}
-
-variable "username" {
-  type        = string
-  description = "The username for the local account that will be created on the new VM."
-  default     = "azureadmin"
+  default     = "myResourceGroup"
 }
