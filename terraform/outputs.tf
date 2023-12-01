d7d2579e38c260d14d9b9f1fb46b9e82ecdaac17
Idriss Khaled <79480567+idrisskhaled@users.noreply.github.com>
2023-12-01 10:59:34 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "public_ip_address" {
   value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
+
+output "key_data" {
+  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).privateKey
+}
