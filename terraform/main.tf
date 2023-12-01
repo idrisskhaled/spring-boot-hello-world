79ef7989bfa984fe74ce11cdf2d0d02511bf0447
Idriss Khaled <79480567+idrisskhaled@users.noreply.github.com>
2023-12-01 13:30:11 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_app_service_plan" "main" {
 }
 
 resource "azurerm_app_service" "main" {
-  name                = "AppService4167"
+  name                = "AppService2023"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   app_service_plan_id = azurerm_app_service_plan.main.id
